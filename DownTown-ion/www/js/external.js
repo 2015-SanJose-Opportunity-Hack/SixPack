2e6af89c48f722f843d1d0351d7e4f060f3a2bc9
sidwarang <siddharth.war@gmail.com>
2015-10-04 00:40:14 -0700

    Ionic code uploaded

---
+++ DownTown-ion/www/js/external.js
@@ -0,0 +1,31 @@
+function loadForm() {
+//    $scope.showHome = false;
+//    $scope.showForm = true;
+//    console.log("set to false");
+    localStorage.setItem('comments',document.getElementsByTagName('textarea')[0].value);
+    window.location.href = "form-view.html";
+}
+
+function uploadData() {
+    var elements = document.getElementsByTagName('input');
+    for (var i=0;i<elements.length;i++) {
+        localStorage.setItem(elements[i].id, elements[i].value);
+    }
+    window.location.href = "confirmation.html";
+}
+
+function returnHome() {
+    
+    window.location.href = "index.html";
+}
+
+function checkLocalData() {
+    var elements = document.getElementsByTagName("input"),
+        element;
+    for (var i=0;i<elements.length;i++) {
+        element = localStorage.getItem(elements[i].id);
+        if(element != "" || element !== undefined) {
+            document.getElementById(elements[i].id).value = element;
+        }
+    }
+}
